19547a61d9552d019396fed7fcb1dd475ad5439e
Trevor D. Brown <trevordylanbrown@gmail.com>
2024-08-07 16:28:02 -0500

    Adjusted script logic
    - Check if directories exists before processing.
    - Doubly process TeX files, if directories are created (so references will work properly).

--- BuildThesis.sh
+++ BuildThesis.sh
@@ -3,13 +3,37 @@
 # Set the root directory of the script.
 THESIS_ROOT=$PWD
 
-# Build the Thesis PDF
+# Check if MetaTeX directory exists.
+if [ -d "$THESIS_ROOT/MetaTeX" ]; then
+    echo "MetaTeX directory exists."
+else
+    mkdir "MetaTeX"
+    echo "MetaTeX directory created."
+
+    # Build the Thesis PDF (will generate, but references are broken.)
+    cd ./Thesis/
+    biber "./../MetaTeX/Thesis.bcf"
+    pdflatex -output-directory="./../MetaTeX/" Thesis.tex
+
+    # Go back to the root directory.
+    cd $THESIS_ROOT
+fi
+
+# Build the Thesis PDF (will generate with references)
 cd ./Thesis/
 biber "./../MetaTeX/Thesis.bcf"
 pdflatex -output-directory="./../MetaTeX/" Thesis.tex
 
 # Go back to the root directory.
 cd $THESIS_ROOT
+
+# Check if Output directory exists.
+if [ -d "$THESIS_ROOT/Output" ]; then
+    echo "Output directory exists."
+else
+    mkdir "Output"
+    echo "Output directory created."
+fi
 
 # Rename the output to the submission filename.
 cp "./MetaTeX/Thesis.pdf" "./Output/Brown.800793873.seas.thesis.pdf"
