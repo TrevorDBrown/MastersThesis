d286b17a8bd5f274feaa6756b613b22e20d6cf5d
Trevor D. Brown <trevordylanbrown@gmail.com>
2024-09-12 06:51:07 -0500

    Updated script to mirror PowerShell script.

--- BuildThesis.sh
+++ BuildThesis.sh
@@ -1,10 +1,38 @@
 #!/bin/zsh
 
-# Set the root directory of the script.
-THESIS_ROOT=$PWD
+# Set the root directory of the script, and other associated paths.
+PROJECT_ROOT=$PWD
+OUTPUT_PATH=$PROJECT_ROOT/Output
+META_PATH=$PROJECT_ROOT/MetaTeX
+THESIS_PATH=$PROJECT_ROOT/Thesis
+
+Build-Thesis() {
+    cd $THESIS_PATH
+
+    # Bibliography Processing
+    echo "Processing Bibliography..."
+    biber "./../MetaTeX/Thesis.bcf"
+
+    # PDF Processing
+    echo "Building PDF..."
+    pdflatex -quiet -output-directory="./../MetaTeX/" Thesis.tex
+
+    # Go back to the root directory.
+    cd $PROJECT_ROOT
+}
+
+Make-Glossaries() {
+    # Make the Glossaries
+    cd $META_PATH
+    echo "Making Glossaries..."
+    makeglossaries -q Thesis
+
+    # Go back to the root directory.
+    cd $PROJECT_ROOT
+}
 
 # Check if Output directory exists.
-if [ -d "$THESIS_ROOT/Output" ]; then
+if [ -d $OUTPUT_PATH ]; then
     echo "Output directory exists."
 else
     mkdir "Output"
@@ -12,28 +40,21 @@ else
 fi
 
 # Check if MetaTeX directory exists.
-if [ -d "$THESIS_ROOT/MetaTeX" ]; then
+if [ -d $META_PATH ]; then
     echo "MetaTeX directory exists."
 else
     mkdir "MetaTeX"
     echo "MetaTeX directory created."
-
-    # Build the Thesis PDF (will generate, but references are broken.)
-    cd ./Thesis/
-    biber "./../MetaTeX/Thesis.bcf"
-    pdflatex -output-directory="./../MetaTeX/" Thesis.tex
-
-    # Go back to the root directory.
-    cd $THESIS_ROOT
 fi
 
-# Build the Thesis PDF (will generate with references)
-cd ./Thesis/
-biber "./../MetaTeX/Thesis.bcf"
-pdflatex -output-directory="./../MetaTeX/" Thesis.tex
+# Build the Thesis PDF (1st pass)
+Build-Thesis
 
-# Go back to the root directory.
-cd $THESIS_ROOT
+# Make the Glossaries
+Make-Glossaries
+
+# Build the Thesis PDF (2nd pass)
+Build-Thesis
 
 # Rename the output to the submission filename.
-cp "./MetaTeX/Thesis.pdf" "./Output/Brown.800793873.seas.thesis.pdf"
+cp "$META_PATH/Thesis.pdf" "$OUTPUT_PATH/Brown.800793873.seas.thesis.pdf"
